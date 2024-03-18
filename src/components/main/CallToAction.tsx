fed5958145f0f79d2e3abc897087182e960ac5b8
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-18 17:02:58 -0600

    Call to action style

--- src/components/main/CallToAction.tsx
+++ src/components/main/CallToAction.tsx
@@ -1,7 +1,8 @@
 import styles from './CallToAction.module.css';
 import imgLogo from '../../images/logo-mastercraft.svg';
-import imgBookmark from '../../images/icon-bookmark.svg';
+import { useState } from 'react';
 export default function CallToAction(): JSX.Element {
+  const [isBookmarked, setIsBookmarked] = useState<boolean>(false);
   return (
     <div className={styles.cta}>
       <img src={imgLogo} className={styles.logo} />
@@ -13,10 +14,27 @@ export default function CallToAction(): JSX.Element {
         <button className={styles.btnBack + ' btn-bluegreen'}>
           Back this project
         </button>
-        <div className={styles.btnBookmarkWrapper}>
-          <img src={imgBookmark} className={styles.imgBookmark} />
+        <div
+          onClick={() => setIsBookmarked((isBookmarked) => !isBookmarked)}
+          className={
+            styles.btnBookmarkWrapper +
+            ' ' +
+            (isBookmarked && styles.bookmarked)
+          }
+        >
+          <svg
+            className={styles.imgBookmark}
+            width='56'
+            height='56'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g fill='none' fill-rule='evenodd'>
+              <circle fill='#2F2F2F' cx='28' cy='28' r='28' />
+              <path fill='#B1B1B1' d='M23 19v18l5-5.058L33 37V19z' />
+            </g>
+          </svg>
           <button className={styles.btnBookmark + ' btn-lightgray'}>
-            Bookmark
+            {isBookmarked ? 'Bookmarked' : 'Bookmark'}
           </button>
         </div>
       </div>
