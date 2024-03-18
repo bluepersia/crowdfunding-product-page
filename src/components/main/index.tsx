c71dc0421f2046f5d4de050997f38294b017e763
Marco Zimmermann <fuzzyslash@hotmail.com>
2024-03-18 16:35:23 -0600

    Header style

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,5 +1,14 @@
+import Backings from './Backings';
+import CallToAction from './CallToAction';
+import Project from './Project';
 import styles from './index.module.css';
 
 export default function Main(): JSX.Element {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <CallToAction />
+      <Backings />
+      <Project />
+    </main>
+  );
 }
